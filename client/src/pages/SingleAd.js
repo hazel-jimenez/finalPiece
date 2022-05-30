820bb308e2082a02711833951e297e87be15ced7
Carlos Vazquez <c.vazquez1986@gmail.com>
2022-05-30 17:10:16 -0400

    added star rating

--- client/src/pages/SingleAd.js
+++ client/src/pages/SingleAd.js
@@ -5,6 +5,7 @@ import { QUERY_AD } from "../utils/queries";
 import ReviewList from "../components/ReviewList";
 import Auth from "../utils/auth";
 import ReviewForm from "../components/ReviewAd";
+import StarRating from "../components/StarRating";
 
 const SingleAd = (props) => {
   const { id: adId } = useParams();
@@ -31,7 +32,9 @@ const SingleAd = (props) => {
           <p>{ad.adText}</p>
         </div>
       </div>
-
+      <div className="card mb-3 flex-row justify-center">
+      <StarRating />
+      </div>
       {ad.reviewCount > 0 && (
         <ReviewList reviews={ad.reviews} />
       )}
